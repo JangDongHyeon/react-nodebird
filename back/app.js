ccf6c9072ebb65e82061432cfaa3b38ccb69f088
jang <jangdonghyeon>
2020-10-25 22:56:39 +0900

    10

--- back/app.js
+++ back/app.js
@@ -48,11 +48,11 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-        domain: process.env.NODE_ENV === 'production' && '.nodebird.com'
-    },
+    // cookie: {
+    //     httpOnly: true,
+    //     secure: false,
+    //     domain: process.env.NODE_ENV === 'production' && '.nodebird.com'
+    // },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
