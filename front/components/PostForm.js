5c64936935219832ced85bf1d92b79c981fd2b48
jang <jangdonghyeon>
2020-10-25 22:50:52 +0900

    8

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { addPost, UPLOAD_IMAGES_REQUEST, REMOVE_IMAGE, ADD_POST_REQUEST } from '../reducers/post';
-
+import { backUrl } from '../config/config'
 const PostForm = props => {
     const { imagePaths, addPostDone } = useSelector(state => state.post);
     const [text, setText] = useState('');
@@ -69,7 +69,7 @@ const PostForm = props => {
                 {imagePaths.map((v, i) => {
                     return (
                         <div key={v} style={{ display: 'inline-block' }}>
-                            <img src={`http://localhost:3065/${v}`} style={{ width: '200px' }} alt={v} />
+                            <img src={`${backUrl}/${v}`} style={{ width: '200px' }} alt={v} />
                             <div>
                                 <Button onClick={onRemoveImage(i)}>제거</Button>
                             </div>
---
+++ front/config/config.js
@@ -0,0 +1,2 @@
+export const backUrl = "http://13.125.93.28"
+export const awsBackUrl = 'http:/13.125.93.28'
